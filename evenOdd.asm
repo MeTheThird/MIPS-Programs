b11e80058550a6ff024c663ea370be8412894b4b
Rohan K. Thakur <rohan.k.thakur@gmail.com>
2021-10-27 11:00:30 -0700

    commented mutliply, added evenOdd

---
+++ evenOdd.asm
@@ -0,0 +1,31 @@
+# EvenOdd takes in an integer as user input and prints Even if the input integer is even, printing
+# Odd otherwise
+# @author Rohan Thakur
+# @version 10/27/21
+    .data
+        oddMsg:     .asciiz "Odd"
+        evenMsg:    .asciiz "Even"
+    .text
+    .globl main
+main:
+    # takes in the integer as user input
+    li $v0, 5
+    syscall
+    move $t0, $v0
+
+    # bitwise ANDs the input integer with 1
+    andi $t0, $t0, 1
+
+    # prints Even if the input integer is even, odd otherwise
+    beq $t0, 1, odd
+    la $a0, evenMsg
+    j after
+odd:
+    la $a0, oddMsg
+after:
+    li $v0, 4
+    syscall
+
+    # normal program termination
+    li $v0, 10
+    syscall
--- multiply.asm
+++ multiply.asm
@@ -1,19 +1,26 @@
+# Multiply takes two integers in as user input and outputs their product
+# @author Rohan Thakur
+# @version 10/27/21
     .data
-    .text 0x00400000
+    .text
     .globl main
 main:
+    # takes in the first integer as user input
     li $v0, 5
     syscall
     move $t0, $v0
 
+    # takes in the second integer as user input
     li $v0, 5
     syscall
     move $t1, $v0
 
+    # outputs the product of the two input integers
     mult $t0, $t1
     mflo $a0
     li $v0, 1
     syscall
 
+    # normal program termination
     li $v0, 10
     syscall
