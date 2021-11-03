b4daf3865b59c20eca89b7766dc940af237b1f56
Rohan K. Thakur <rohan.k.thakur@gmail.com>
2021-11-03 10:31:58 -0700

    completed UserGuessNum.asm

--- UserGuessNum.asm
+++ UserGuessNum.asm
@@ -4,3 +4,50 @@
 #
 # @author Rohan Thakur
 # @version 11/2/21
+    .data
+guessQuestion:  .asciiz "What is your integer guess? "
+tooHigh:    .asciiz "Too high\n"
+tooLow:     .asciiz "Too low\n"
+success:     .asciiz "Success!\n"
+    .text
+    .globl main
+main:
+    # generates and stores a random number
+    li $v0, 41
+    syscall
+    move $t0, $a0
+
+    # prints the generated random number for testing purposes
+    # li $v0, 1
+    # syscall
+
+guess:
+    # asks the user for an integer guess
+    la $a0, guessQuestion
+    li $v0, 4
+    syscall
+    # reads in a user-input integer
+    li $v0, 5
+    syscall
+    # prints success and terminates the program if the user guesses correctly
+    beq $v0, $t0, victory
+    # prints out whether the user's guess was too high or too low
+    bgt $v0, $t0, tooLarge
+    la $a0, tooLow
+    # asks for the user's guess again
+    j guessAgain
+tooLarge:
+    la $a0, tooHigh
+guessAgain:
+    li $v0, 4
+    syscall
+    j guess
+victory:
+    # prints the success message
+    la $a0, success
+    li $v0, 4
+    syscall
+
+    # normal program termination
+    li $v0, 10
+    syscall
